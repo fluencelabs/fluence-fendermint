7e3cf1d31242aab580cfc3d19215085d8a594b2d
Mike Voronov <michail.vms@gmail.com>
2023-12-11 14:01:15 +0300

    switch dependecy to git

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,9 @@ mod sys;
 
 pub use sys::RANDOMX_HASH_SIZE;
 
-pub fn run_randomx(k: Vec<u8>, h: Vec<u8>) -> Result<[u8; RANDOMX_HASH_SIZE], fvm_shared::error::ErrorNumber> {
-    unsafe {
-        sys::run_randomx(k.as_ptr(), h.len() as u32, h.as_ptr(), h.len() as u32)
-    }
+pub fn run_randomx(
+    k: Vec<u8>,
+    h: Vec<u8>,
+) -> Result<[u8; RANDOMX_HASH_SIZE], fvm_shared::error::ErrorNumber> {
+    unsafe { sys::run_randomx(k.as_ptr(), h.len() as u32, h.as_ptr(), h.len() as u32) }
 }
