13cec36c5e67aa64a82885428da4fbc50790c96a
Mike Voronov <michail.vms@gmail.com>
2024-01-28 14:42:30 +0300

    move to FVM 4.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,33 @@
-mod fvm;
+/*
+ * Copyright 2024 Fluence Labs Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 mod sys;
 
 pub use sys::RANDOMX_HASH_SIZE;
 
 pub fn run_randomx(
-    k: Vec<u8>,
-    h: Vec<u8>,
+    global_nonce: Vec<u8>,
+    local_nonce: Vec<u8>,
 ) -> Result<[u8; RANDOMX_HASH_SIZE], fvm_shared::error::ErrorNumber> {
-    unsafe { sys::run_randomx(k.as_ptr(), h.len() as u32, h.as_ptr(), h.len() as u32) }
+    unsafe {
+        sys::run_randomx(
+            global_nonce.as_ptr(),
+            global_nonce.len() as u32,
+            local_nonce.as_ptr(),
+            local_nonce.len() as u32,
+        )
+    }
 }
--- src/sys.rs
+++ src/sys.rs
@@ -1,4 +1,20 @@
-use crate::fvm::fvm_syscalls;
+/*
+ * Copyright 2024 Fluence Labs Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use fvm_sdk::sys::fvm_syscalls;
 
 pub const RANDOMX_HASH_SIZE: usize = 32;
 
@@ -6,9 +22,9 @@ fvm_syscalls! {
     module = "fluence";
 
     pub fn run_randomx(
-        k_addr: *const u8,
-        k_len: u32,
-        h_addr: *const u8,
-        h_len: u32,
+        global_nonce_addr: *const u8,
+        global_nonce_len: u32,
+        local_nonce_addr: *const u8,
+        local_nonce_len: u32,
     ) -> Result<[u8; RANDOMX_HASH_SIZE]>;
 }
